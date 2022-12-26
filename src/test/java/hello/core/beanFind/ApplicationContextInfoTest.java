337b264e9fcabe2a608b8cd5d9db8e8ccc4f60c9
jisu <jisuuoo96@gmail.com>
2022-12-26 13:35:36 +0900

    :white_check_mark: 컨테이너에 등록된 빈 조회 테스트 추가 - 모든 빈 조회 - 애플리케이션 빈 조회 (직접 등록한 애플리케이션 빈)

---
+++ src/test/java/hello/core/beanFind/ApplicationContextInfoTest.java
@@ -0,0 +1,37 @@
+package hello.core.beanFind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName); // 타입을 지정하지 않았기 때문에 타입이 Object로 꺼내지게 된다.
+            System.out.println("name = " + beanDefinitionName + "object = " + bean);
+        }
+    }
+
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println(" name = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+    }
+}

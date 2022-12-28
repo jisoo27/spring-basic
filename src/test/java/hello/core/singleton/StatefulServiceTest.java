e0a0f004a59d8cc428de4cb502c86aa4dd588b92
jisu <jisuuoo96@gmail.com>
2022-12-28 18:52:07 +0900

    :white_check_mark: 싱글톤 객체를 무상태(stateless) 설계 했을 경우 테스트 추가 - 지역변수 활용

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -21,10 +21,28 @@ class StatefulServiceTest {
         statefulService2.order("userB", 20000);
 
         // ThreadA : 사용자A가 주문 금액을 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
+        //int price = statefulService1.getPrice();
+        //System.out.println("price = " + price);
 
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        //Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    @Test
+    void statelessServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        // ThreadA : A사용자가 만원을 주문
+        int userAPrice = statefulService1.order("userA", 10000);
+        // ThreadB : B사용자가 2만원을 주문
+        int userBPrice = statefulService2.order("userB", 20000);
+
+        // ThreadA : 사용자A가 주문 금액을 조회
+        //int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice);
+
+        Assertions.assertThat(userAPrice).isEqualTo(10000);
     }
 
     @Configuration

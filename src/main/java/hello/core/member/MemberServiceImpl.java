a20a787d683538c06996d1ff6c1bcb547029e581
jisu <jisuuoo96@gmail.com>
2023-01-02 17:16:28 +0900

    :recycle: @ComponentScan 과 의존관계 자동 주입(@Autowired) 사용하여 코드 변경 및 테스트 추가

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired // 자동으로 ac.getBean(MemberRepository.class) 이 코드가 들어간다고 볼 수 있다.
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }

bd05663acd52590d12f2434e554a4874bddf2de9
jisu <jisuuoo96@gmail.com>
2023-01-04 15:48:37 +0900

    :white_check_mark: 생성자 주입을 사용할 경우 순수한 자바 코드를 사용하여 테스트 가능 예제 추가

---
+++ src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,24 @@
+package hello.core.order;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemoryMemberRepository;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder() { // 생성자 주입을 사용하면 순수한 자바 코드로 테스트를 만들 수 있다.
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}

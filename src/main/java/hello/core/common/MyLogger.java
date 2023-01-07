8539dbecf36191d04fbbdcdff64d22c306ebd84f
jisu <jisuuoo96@gmail.com>
2023-01-07 15:58:48 +0900

    :recycle: 프록시를 사용하여 문제 해결

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,19 +9,13 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger { // 로그를 출력하기 위한 클래스
-    /*
-    * @Scope(value = "request")를 사용해서 request 스코프로 지정했다.
-    * 이게 이 빈은 HTTP 요청 당 하나씩 생성되고, HTTP 요청이 끝나는 시점에 소멸된다.
-     * */
-
 
     private String uuid;
     private String requestURL;
 
     public void setRequestURL(String requestURL) {
-        // requestURL 은 이 빈이 생성되는 시점에는 알 수 없으므로, 외부에서 setter로 입력받는다.
         this.requestURL = requestURL;
     }
 
@@ -30,9 +25,7 @@ public class MyLogger { // 로그를 출력하기 위한 클래스
 
     @PostConstruct
     public void init() {
-        // 이 빈이 생성되는 시점에 자동으로 초기화 메서드를 사용해서 uuid를 생성해서 저장해둔다.
-        // 이 빈은 HTTP 요청 당 하나씩 생성되므로, uuid를 저장해두면 다른 HTTP 요청과 구분할 수 있다.
-        uuid = UUID.randomUUID().toString(); // 절때로 겹치치지 않는 id를 생성할 수 있다.
+        uuid = UUID.randomUUID().toString();
         System.out.println("[" + uuid + "] request scope bean create:" + this);
 
     }
